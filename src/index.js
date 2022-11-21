3a72daac34423610cc572a58372e2733a97e341e
RollsRoycewk <50765560@qq.com>
2022-11-21 22:06:18 +0800

    feat: 添加忽略文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Test from "./components/Test/index";
-import Test2 from "./components/Test2/index";
-import Test3 from "./components/Test3/index";
+// import Test from "./components/Test/index";
+// import Test2 from "./components/Test2/index";
+// import Test3 from "./components/Test3/index";
 import Icon from "./components/Icon/index";
 import SvgAnimation from  "./components/SvgAnimation/index";
 import JanLoading from "./components/JanLoading/index";
@@ -9,9 +9,9 @@ import Container from "./components/Container/index"
 import JanLogo from "./components/JanLogo"
 
 export default function (Vue) {
-    Vue.use(Test);
-    Vue.use(Test2);
-    Vue.use(Test3);
+    // Vue.use(Test);
+    // Vue.use(Test2);
+    // Vue.use(Test3);
     Vue.use(Icon);
     Vue.use(SvgAnimation);
     Vue.use(JanLoading);
