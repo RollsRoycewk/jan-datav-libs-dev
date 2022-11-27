c7f20bfdd9f5c3724f9e5de64b6f2ce0ed3d57b2
RollsRoycewk <50765560@qq.com>
2022-11-27 19:46:46 +0800

    feat: 轮播组件-行数据渲染

--- src/components/BaseScrollList/BaseScrollList.vue
+++ src/components/BaseScrollList/BaseScrollList.vue
@@ -13,7 +13,20 @@
            v-html="headerItem"
       ></div>
     </div>
-    <div class="base-scroll-list-rows"></div>
+    <div class="base-scroll-list-rows"
+         v-for="(rowData,rowIndex) in rowsData"
+         :key="rowIndex">
+
+          <div
+            class="base-scroll-list-columns"
+            v-for="(colData,colIndex) in rowData"
+            :key="colData + colIndex"
+            v-html="colData"
+            :style="{
+             width:`${columnWidths[colIndex]}px`
+           }"
+          ></div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +36,25 @@ import { v4 as uuidv4 } from "uuid"
 import useScreen from "../../hooks/useScreen"
 import cloneDeep from "lodash/cloneDeep"
 import assign from "lodash/assign"
+
+const defaultConfig = {
+  // 标题数据,格式: ['a','b','c']
+  headerData:[],
+  // 标题样式,格式[{},{},{}]
+  headerStyle:[],
+  // 标题的背景色
+  headerBg:"rgb(90,90,90)",
+  // 标题的高度
+  headerHeight:35,
+  // 标题是否展示序号
+  headerIndex:false,
+  headerIndexContent:"#",
+  headerIndexStyle:{
+    width:'50px'
+  },
+  // 数据项,二维数组
+  data:[]
+}
 
 export default {
   name: "BaseScrollList",
@@ -40,30 +72,14 @@ export default {
     const headerStyle = ref([])
     const actualConfig = ref({})
     const columnWidths = ref([])
-
-
-    const defaultConfig = {
-      // 标题数据,格式: ['a','b','c']
-      headerData:[],
-      // 标题样式,格式[{},{},{}]
-      headerStyle:[],
-      // 标题的背景色
-      headerBg:"rgb(90,90,90)",
-      // 标题的高度
-      headerHeight:35,
-      // 标题是否展示序号
-      headerIndex:false,
-      headerIndexContent:"#",
-      headerIndexStyle:{
-        width:'50px'
-      }
-    }
+    const rowsData = ref([])
 
 
     const handleHeader = (config) => {
       // 因为每一次对原数据的操作会触发重新渲染
       const _headerData = cloneDeep(config.headerData)
       const _headerStyle = cloneDeep(config.headerStyle)
+      const _rowsData = cloneDeep(config.data)
 
       if (_headerData.length === 0){
         return
@@ -72,31 +88,55 @@ export default {
       if (config.headerIndex){
         _headerData.unshift(config.headerIndexContent)
         _headerStyle.unshift(config.headerIndexStyle)
+        _rowsData.forEach((rows,index) => {
+          rows.unshift(index+1)
+        })
       }
 
       // 动态计算header中的每一列的宽度
       let usedWidth = 0;
       let usedColumnNum = 0;
       // 判断是否自定义width
-      _headerStyle.forEach((style) => {
-        if (style.width){
-          usedWidth += +style.width.replace('px','')
-          usedColumnNum ++
-        }
-      })
+      // _headerStyle.forEach((style) => {
+      //   if (style.width){
+      //     usedWidth += +style.width.replace('px','')
+      //     usedColumnNum ++
+      //   }
+      // })
       // 动态计算列宽时,使用剩余的宽度除以剩余的列数
       const avgWidth = (width.value - usedWidth) / (_headerData.length - usedColumnNum);
       const _columnWidth= new Array(_headerData.length).fill(avgWidth)
 
+      _headerStyle.forEach((style,index) => {
+        if (style.width){
+          // usedWidth += +style.width.replace('px','')
+          const headerWidth = +style.width.replace('px','')
+          usedWidth += headerWidth
+          usedColumnNum ++
+          _columnWidth[index] = headerWidth;
+        }
+      })
+
       columnWidths.value = _columnWidth
 
       headerData.value = _headerData;
+      rowsData.value = _rowsData;
       headerStyle.value = _headerStyle
+    }
+
+    const handleRows = (config) => {
+      // 动态计算每行数据的高度
+
+
+      console.log("config",rowsData.value)
     }
 
     onMounted(() => {
       const _actualConfig = assign(defaultConfig,props.config)
+
+      rowsData.value = _actualConfig.data || [];
       handleHeader(_actualConfig)
+      handleRows(config);
 
       actualConfig.value = _actualConfig;
     })
@@ -105,7 +145,8 @@ export default {
       id,
       headerData,
       headerStyle,
-      columnWidths
+      columnWidths,
+      rowsData
     }
   }
 }
@@ -131,6 +172,14 @@ export default {
 
      .header-item{
 
+     }
+   }
+
+   .base-scroll-list-rows{
+     display: flex;
+
+     .base-scroll-list-columns{
+       //font-size: 28px;
      }
    }
  }
